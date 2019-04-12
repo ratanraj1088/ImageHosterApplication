8804fa7179b04d6a249a7ad843541d70800fefe1
ratanraj1088 <ratanraj2212@gmail.com>
2019-04-13 00:22:31 +0530

    Part A: Fixing Issues:Image upload issues

--- src/main/java/ImageHoster/repository/ImageRepository.java
+++ src/main/java/ImageHoster/repository/ImageRepository.java
@@ -50,16 +50,26 @@ public class ImageRepository {
     //Executes JPQL query to fetch the image from the database with corresponding title
     //Returns the image in case the image is found in the database
     //Returns null if no image is found in the database
-    public Image getImageByTitle(String title) {
+  // public Image getImageByTitle(String title) {
+  //     EntityManager em = emf.createEntityManager();
+  //     try {
+  //         TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+  //         return typedQuery.getSingleResult();
+  //     } catch (NoResultException nre) {
+  //         return null;
+  //     }
+  // }
+
+
+    public Image getImageByTitle(Integer imageId) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.title =:title", Image.class).setParameter("title", title);
+            TypedQuery<Image> typedQuery = em.createQuery("SELECT i from Image i where i.id =:imageId", Image.class).setParameter("imageId", imageId);
             return typedQuery.getSingleResult();
         } catch (NoResultException nre) {
             return null;
         }
     }
-
     //The method creates an instance of EntityManager
     //Executes JPQL query to fetch the image from the database with corresponding id
     //Returns the image fetched from the database
