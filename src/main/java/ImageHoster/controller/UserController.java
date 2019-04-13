5259939b8b69b9d9eb5d817c874c6586f3964c54
ratanraj1088 <ratanraj2212@gmail.com>
2019-04-14 00:59:04 +0530

    Part B:Implement a new feature:Password Strength

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -40,9 +41,20 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model, final RedirectAttributes redirectAttributes) {
+        String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+        if(user.getPassword().matches(".*[a-zA-Z]+.*[0-9]+.*[^\\w\\s]+.*")){
+            userService.registerUser(user);
+            return "redirect:/users/login";
+
+        }
+        else {
+            redirectAttributes.addAttribute("passwordTypeError", error);
+            model.addAttribute("passwordTypeError", error);
+            redirectAttributes.addFlashAttribute("passwordTypeError", error);
+            return "redirect:/users/registration";
+        }
+
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
